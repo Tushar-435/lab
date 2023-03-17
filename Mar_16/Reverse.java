1374814575b05b0e44b9c73e5b9fcc714841df45
Tushar-435 <122785295+Tushar-435@users.noreply.github.com>
2023-03-17 15:21:38 +0530

    Add files via upload

---
+++ Mar_16/Reverse.java
@@ -0,0 +1,9 @@
+package Mar_16;
+
+public class Reverse {
+	
+		public static void main(String[] args) { // main method 
+			StringBuffer s=new StringBuffer("My Name is Tushar"); //take string buffer 
+			s.reverse();
+			System.out.println("After reversing :"+s); //print the output after reverse
+		}}
